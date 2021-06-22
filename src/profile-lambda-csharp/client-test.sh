3c6d55203c0f5e1aeeb153e65275da95ad471e09
eisnerw <eisnerw@hotmail.com>
2021-06-22 15:21:02 -0400

    localstack breaking change;Fix environment setting

--- src/profile-lambda-csharp/client-test.sh
+++ src/profile-lambda-csharp/client-test.sh
@@ -21,10 +21,18 @@ export RESTAPI=$(awslocal apigateway get-rest-apis | ./json "items.0.id")
 echo testing POST...
 curl -X POST "http://localhost:4566/restapis/${RESTAPI}/local/_user_request_/profile" -H 'Content-Type: application/json' --data '@test.data'
 echo
-awslocal s3api list-objects --bucket profile-pictures | ./json "Contents.Key" grep -q 'my-profile2.pic.jpg' && echo 'S3 WAS LOADED SUCCESSFULLY'
-awslocal dynamodb scan --table-name Profiles | ./json "Items.0.Email.S" grep -q 'kulubettin@gmail.com' && echo 'DYNAMODB WAS LOADED SUCCESSFULLY'
-
+if (awslocal s3api list-objects --bucket profile-pictures | ./json "Contents.0.Key" | grep -q 'my-profile2.pic.jpg';) then
+    echo "S3 WAS LOADED SUCCESSFULLY BY POST";
+else
+    echo "S3 WAS NOT LOADED BY POST!!";
+fi    
+if (awslocal dynamodb scan --table-name Profiles | ./json "Items.0.Email.S" | grep -q 'kulubettin@gmail.com';) then
+    echo 'DYNAMODB WAS LOADED SUCCESSFULLY BY POST';
+else 
+    echo 'DYNAMODB WAS NOT LOADED BY POST!!';
+fi
 echo resetting the aws environment
+echo
 awslocal s3 rb s3://profile-pictures  --force > /dev/null # delete bucket if it exists 
 awslocal s3api create-bucket --bucket profile-pictures --region eu-central-1
 awslocal dynamodb delete-table --table-name Profiles > /dev/null # delete table if it exists
@@ -33,8 +41,15 @@ awslocal dynamodb create-table --table-name Profiles --attribute-definitions Att
 echo testing aws-cli invoke
 awslocal lambda invoke --cli-binary-format raw-in-base64-out --function-name profile-local-hello --payload "$(< testEscaped.data)" response.json --log-type Tail | ./json "LogResult" | base64 --decode
 echo
-awslocal s3api list-objects --bucket profile-pictures | ./json "Contents.Key" grep -q 'my-profile2.pic.jpg' && echo 'S3 WAS LOADED SUCCESSFULLY'
-awslocal dynamodb scan --table-name Profiles | ./json "Items.0.Email.S" grep -q 'kulubettin@gmail.com' && echo 'DYNAMODB WAS LOADED SUCCESSFULLY'
-
+if (awslocal s3api list-objects --bucket profile-pictures | ./json "Contents.0.Key" | grep -q 'my-profile2.pic.jpg';) then
+    echo "S3 WAS LOADED SUCCESSFULLY BY CLI";
+else
+    echo "S3 WAS NOT LOADED BY CLI!!";
+fi    
+if (awslocal dynamodb scan --table-name Profiles | ./json "Items.0.Email.S" | grep -q 'kulubettin@gmail.com';) then
+    echo 'DYNAMODB WAS LOADED SUCCESSFULLY BY CLI';
+else 
+    echo 'DYNAMODB WAS NOT LOADED BY CLI!!';
+fi
 
 
